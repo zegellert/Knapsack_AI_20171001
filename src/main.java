3cdf0a8bbbdc7a4018d23c70f572751872df3a59
zegellert <zegellert@gmail.com>
2017-10-19 08:42:37 +0200

    Cumulative system implemented
    Also, started working on the first version of a fitting algorithm.

--- src/main.java
+++ src/main.java
@@ -14,44 +14,83 @@ import java.util.ArrayList;
  *
  * @author Gellert Zelenak
  */
-public class main {
+public class Main {
     
     static model m;
     static ArrayList<item> items=new ArrayList<item>();
+    static ArrayList<item> strays=new ArrayList<item>();
+    static ArrayList<cumulative> cumulatives=new ArrayList<cumulative>();
+    static int number=0;
 
    
     public static void main(String[] args) {
-       System.out.println("JUPITER");
+       
      
        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
        try{
-       System.out.println("Enter the width/height of the sack:");
+       
        String size=reader.readLine();
        m=new model(Integer.parseInt(size.split("\t")[0]),Integer.parseInt(size.split("\t")[1]));
-       System.out.println("Enter the number of items:");
-       int number=Integer.parseInt(reader.readLine());
+       for(int i=0;i<m.height;i++){
+           cumulative c=new cumulative(m.height);
+           cumulatives.add(c);
+       }
+       
+       number=Integer.parseInt(reader.readLine());
        for (int i=0;i<number;i++){
-           System.out.println("Enter item#"+(i+1)+" width/height:");
+          
            String item_s=reader.readLine();
            item temp=new item(Integer.parseInt(item_s.split("\t")[0]),Integer.parseInt(item_s.split("\t")[1]),i+1);
            items.add(temp);
        }
        }
-       catch(Exception e){System.out.println("There was an error. Message: "+e.getMessage());}
+       catch(Exception e){}
        
-       item tester=items.get(3);
+       item_to_sack(items.get(0),m,0,0);
        view.display_model(m);
-       item_to_sack(tester,m,0,0);
-       view.display_model(m);
-      
-      
-       System.out.println("END");
+       display_cumulatives(cumulatives);
        
+       
+       
+      
+
+       
+    }
+    
+    public static void try_to_fit(item item,model sack,int X,int Y){
+        if(X>sack.width){strays.add(item);}
+        else{
+        int column=0;
+        int row=0;
+        
+        ///THIS OUGHT TO BE DONE IN A WHILE LOOP:
+        for(int i=0;i<cumulatives.size();i++){
+            cumulative c=cumulatives.get(i);
+            if(item.height<c.space){
+                column=i;
+                row=c.next;
+                break;
+            }
+        }
+        }
+        
+        
+        
+        
+      
+        
+        
+        
     }
     
     public static void item_to_sack(item item,model sack,int X,int Y){
         try{
             sack.insert(X, Y, item);
+            for(int i=X;i<X+item.width;i++){
+                cumulative c=cumulatives.get(i);
+                c.space-=item.height;
+                c.next=Y+item.height+1;
+            }
         
         }
         catch(Exception e){
@@ -59,4 +98,28 @@ public class main {
             System.out.println("That item couldn't be inserted there. Error: "+e.getMessage());}
     }
     
+    ///OUTSOURCE THIS TO VIEW!!!!!...OR DELETE IT, AS SEE FIT
+    public static void display_cumulatives(ArrayList<cumulative> list){
+        System.out.print("Cumulatives: ");
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i).space+":"+list.get(i).next+",");
+        }
+        System.out.print("\n");
+    }
+    
+    
+    public static class cumulative{
+        int space=0;
+        int next=0;
+        
+        public cumulative(int s){
+            space=s;
+        }
+    }
+    
+    
+    
 }
+
+
+
